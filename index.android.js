7590c99b7fbb70db455834dd65e618d2cc06ac60
Evgeniy Troynov <troinov174@gmail.com>
2016-08-19 15:17:43 +0500

    Initial commit

---
+++ index.android.js
@@ -0,0 +1,10 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import {AppRegistry} from 'react';
+import Root from './components/Root.js';
+
+AppRegistry.registerComponent('stopgok', () => Root);
